a661593d5cadb2226065aa493d7d4a8428725e76
Remi-Duquesnay <67142238+Remi-Duquesnay@users.noreply.github.com>
2020-09-18 16:46:47 +0200

    Add files via upload

--- php/niveau-2/PHP-Exo5-Conjugaison/index.php
+++ php/niveau-2/PHP-Exo5-Conjugaison/index.php
@@ -8,14 +8,17 @@
 		
 		<?php
 			function conjugaison($str){
-				$index = strlen($str)-1;
-				$strEnd = $str[$index-1] . $str[$index];
-				if($index<=15 && !preg_match('/\s/',$str) && $strEnd == "er" ){
-					$newStr = substr($str,0,$index-1);
+				$size = strlen($str)-1;
+				if($size<=15 && !preg_match('/\s/',$str)){
+					$newStr = substr($str,0,$size-1) ;
 					echo "Je " . $newStr ."e <br>";
 					echo "Tu " . $newStr ."es <br>";
 					echo "Il/elle " . $newStr ."e <br>";
-					echo "Nous " . $newStr ."ons <br>";
+					if(substr($str, $size-2)== "ger" ){
+						echo "Nous " . $newStr ."eons <br>";
+					}else{
+						echo "Nous " . $newStr ."ons <br>";
+					}
 					echo "Vous " . $newStr ."ez <br>";
 					echo "Ils/elles " . $newStr ."ent <br><br>";
 				}
