b559a275c45c02a7e543a9b51e32109e34a75dd8
Remi-Duquesnay <rduquesnay@orange.fr>
2020-09-21 23:18:54 +0200

    commit

--- php/niveau-3/Bonus - Traitement_de_formulaire + exercices par Micky/cards.php
+++ php/niveau-3/Bonus - Traitement_de_formulaire + exercices par Micky/cards.php
@@ -1,10 +1,27 @@
 <?php
-    function card($title, $nbVotes, $totalVoteScore, $imgLocation){
-        echo "<div class=\"card\" style=\"width:400px\">";
-        echo "<img class=\"card-img-top\" src=\"".$imgLocation."\" alt=\"".$title."-cover\">";
-        echo "<div class=\"card-body\">";
-        echo "<h4 class=\"card-title\">".$title."</h4>";
-        echo "<p class=\"card-text\">Nombre de votes : ".$nbVotes."</p>";
-        echo "</div>";
-        echo "</div>";
+function card($id, $title, $nbVotes, $totalVoteScore, $imgLocation, $cookieVote)
+{
+    echo "<div class=\"card\" style=\"width:300px\">";
+    echo "<img class=\"card-img-top game-cover\" src=\"" . $imgLocation . "\" alt=\"" . $title . "-cover\">";
+    echo "<div class=\"card-body\">";
+    echo "<h4 class=\"card-title\">" . $title . "</h4>";
+    // if (!$cookieVote[$id]) {
+        echo "<form action=\"note.php\" method=\"POST\">";
+        echo "<p>Noter ce jeu : <select name=\"note\">";
+        for ($i = 0; $i <= 5; $i++) {
+            echo "<option value=\"" . $i . "\">".$i."</option>";
+        }
+        echo "</select>";
+        echo "<button type=\"submit\" name=\"submit\">Noter</button>";
+    // }
+    echo "<div class=\"card-note\">";
+    if ($nbVotes == 0) {
+        echo "<p class=\"card-text\">Note : pas encore noté.</p>";
+    } else {
+        $noteAvg = $totalVoteScore / $nbVotes;
+        echo "<p class=\"card-text\">Note : " . $noteAvg . "(" . $nbVotes . " votes)</p>";
     }
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+}
--- php/niveau-3/Bonus - Traitement_de_formulaire + exercices par Micky/index.php
+++ php/niveau-3/Bonus - Traitement_de_formulaire + exercices par Micky/index.php
@@ -1,14 +1,16 @@
-
 <?php
 if (isset($_COOKIE["vote"])) {
 	$votes = $_COOKIE["vote"];
+}else{
+	$votes="";
 }
+setcookie("gameVotes", $votes, time() + 60*60*24);
 // connection to the data base.
-$connDB = mysqli_connect("localhost","virtual","1234","game_votes");
+$connDB = mysqli_connect("localhost", "virtual", "1234", "game_votes");
 
 // check of the connection
-if(!$connDB){
-	echo "Erreur de connexion : ". mysqli_connect_error();
+if (!$connDB) {
+	echo "Erreur de connexion : " . mysqli_connect_error();
 }
 
 // Create querry for the data
@@ -24,7 +26,6 @@ $games = mysqli_fetch_all($result, MYSQLI_ASSOC);
 mysqli_free_result($result);
 mysqli_close($connDB);
 
-print_r($games);
 
 ?>
 <!DOCTYPE html>
@@ -37,6 +38,7 @@ print_r($games);
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title></title>
 	<?php
 	include("cards.php");
@@ -44,15 +46,17 @@ print_r($games);
 </head>
 
 <body>
-	<?php
-	echo "<br>";
-	echo "<br>";
-	echo "<br>";
-
-	foreach($games as $index => $value){
-		card($value["title"],$value["number_of_votes"],$value["total_vote_score"],$value["img_location"]);
-	}
-	?>
+	<div class="container">
+		<div class="row">
+			<div class="card-columns">
+				<?php
+				foreach ($games as $index => $value) {
+					card($value["id"],$value["title"], $value["number_of_votes"], $value["total_vote_score"], $value["img_location"], $votes);
+				}
+				?>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
