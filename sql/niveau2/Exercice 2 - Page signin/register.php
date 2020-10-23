129c053b18b6937191ab3356705b223f2661d9c9
Remi-Duquesnay <rduquesnay@orange.fr>
2020-10-23 09:20:37 +0200

    commit

--- sql/niveau2/Exercice 2 - Page signin/register.php
+++ sql/niveau2/Exercice 2 - Page signin/register.php
@@ -9,55 +9,101 @@ function test_input($data)
     return $data;
 }
 
-$nameErr = $surnameErr = $emailErr = $passwordErr = "";
+function verifEmailDispo($email)
+{
+    $dbUser = 'root';
+    $dbPass = '';
+
+    $dbh = new mysqli('localhost', $dbUser, $dbPass, 'niveau2');
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    $sql = "SELECT email FROM Utilisateurs WHERE email = ? ";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $result = $stmt->fetch();
+    return $result;
+}
+
+function signingUp($name, $lastname, $email, $password, $pro)
+{
+    $dbUser = 'root';
+    $dbPass = '';
+
+    $dbh = new mysqli('localhost', $dbUser, $dbPass, 'niveau2');
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    $querryLoginIn = "INSERT INTO Utilisateurs(name, lastname, email, password, pro) VALUES (?,?,?,?,?)";
+    $stmt = $dbh->prepare($querryLoginIn);
+    $stmt->bind_param('ssssi', $name, $lastname, $email, $password, $pro);
+    $stmt->execute();
+    $result = $stmt->fetch();
+}
+
+$nameErr = $lastnameErr = $emailErr = $passwordErr = $passwordConfirmErr = $proErr = $cguErr = "";
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = test_input($_POST["name"]);
+    $lastname = test_input($_POST["lastname"]);
+    $email = test_input($_POST["email"]);
+    $password = test_input($_POST["password"]);
+    $passwordConfirm = test_input($_POST["passwordConfirm"]);
+
     $valid = true;
-    if (empty($_POST["name"])) {
+    if (empty($name)) {
         $nameErr = "Veuillez indiquer votre nom de famille.";
         $valid = false;
     }
-    if (empty($_POST["surname"])) {
-        $surnameErr = "Veuillez indiquer votre prénom.";
+    if (empty($lastname)) {
+        $lastnameErr = "Veuillez indiquer votre prénom.";
         $valid = false;
     }
-    if(empty($_POST["email"])) {
+    if (empty($email)) {
         $emailErr = "Veuillez indiquer un E-mail";
         $valid = false;
-        }else{
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    } else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Veuillez indiquer un E-mail valide.";
             $valid = false;
+        } else {
+            if (verifEmailDispo($email)) {
+                $valid = false;
+                $emailErr = "Cet E-mail est déjà utilisé!";
+            }
         }
     }
-    if (empty($_POST["password"])) {
+    if (empty($password)) {
         $passwordErr = "Veuillez indiquer un mot de passe.";
         $valid = false;
-    }else[
-        
-    ]
+    } else {
+        if (strlen($password) < 8 || !preg_match("~[0-9]~", $password) ||  !preg_match("~[a-z]~", $password) ||  !preg_match("~[A-Z]~", $password)) {
+            $passwordErr = "Votre mot de passe doit contenir minimum : 8 charactères, un chiffre, une majuscule et une muniscule";
+            $valid = false;
+        } else {
+            if ($passwordConfirm != $password) {
+                $passwordConfirmErr = "Les mots de passe ne correspondent pas.";
+                $valid = false;
+            }
+        }
+    }
+    if (!isset($_POST["pro"])) {
+        $proErr = "Veuillez selectioner un status.";
+    }
+    if (!isset($_POST["cgu"])) {
+        $cguErr = "Veuillez lire et accepter les conditions d'utilisation";
+    }
+
+
 
     if ($valid == true) {
-        $username = test_input($_POST["username"]);
-        echo $username;
-        $password = test_input($_POST["password"]);
-        echo $password;
-        $date = date("Y-m-d H:i:s");
-        echo $date;
-
-        $dbUser = 'root';
-        $dbPass = '';
-        try {
-            $dbh = new PDO('mysql:host=localhost;dbname=niveau2exo1', $dbUser, $dbPass);
-            $sql = "INSERT INTO Connexions (login, password, date_connexion)
-                    VALUES  ('$username', '$password', '$date')";
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute();
-            $dbh = null;
-        } catch (PDOException $e) {
-            echo "Erreur !: " . $e->getMessage() . "<br/>";
-            die();
-        }
+        $pro = $_POST["pro"];
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        signingUp($name, $lastname, $email, $password, $pro);
     }
 }
 
@@ -68,7 +114,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style .error{color:red;}></style>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
     <title>sql - login page</title>
 </head>
 
@@ -79,8 +129,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <label for="name">Nom :</label>
         <input type="text" id="name" name="name" value="<?php echo isset($_POST["name"]) ? $_POST["name"] : "" ?>"><span class="error"><?php echo $nameErr; ?></span>
         <br>
-        <label for="surname">Prénom :</label>
-        <input type="text" id="surname" name="surname" value="<?php echo isset($_POST["surname"]) ? $_POST["surname"] : "" ?>"><span class="error"><?php echo $surnameErr; ?></span>
+        <label for="lastname">Prénom :</label>
+        <input type="text" id="lastname" name="lastname" value="<?php echo isset($_POST["lastname"]) ? $_POST["lastname"] : "" ?>"><span class="error"><?php echo $lastnameErr; ?></span>
         <br>
         <label for="email">E-mail :</label>
         <input type="text" id="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : "" ?>"><span class="error"><?php echo $emailErr; ?></span>
@@ -88,16 +138,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password"><span class="error"><?php echo $passwordErr; ?></span>
         <br>
-        <label for="passwordConfirm">Mot de passe :</label>
-        <input type="password" id="passwordConfirm" name="passwordConfirm"><span class="error"><?php echo $passwordErr; ?></span>
+        <label for="passwordConfirm">Confirmation du mot de passe :</label>
+        <input type="password" id="passwordConfirm" name="passwordConfirm"><span class="error"><?php echo $passwordConfirmErr; ?></span>
         <br>
         <label for="pro">Je suis : professionel</label>
-        <input type="radio" id="pro" name="pro" value="true" <?php if(isset($_POST['pro']) && $_POST['pro'] == 'true'){echo ' checked="checked"';} ?>>
+        <input type="radio" id="pro" name="pro" value="pro" <?php if (isset($_POST['pro']) && $_POST['pro'] == 'pro') {
+                                                                    echo ' checked="checked"';
+                                                                } ?>>
         <label for="particulier"> particulier</label>
-        <input type="radio" id="particulier" name="pro" value="false" <?php if(isset($_POST['pro']) && $_POST['pro'] == 'false'){echo ' checked="checked"';} ?>>
+        <input type="radio" id="particulier" name="particulier" value="false" <?php if (isset($_POST['pro']) && $_POST['pro'] == 'particulier') {
+                                                                            echo ' checked="checked"';
+                                                                        } ?>>
+        <span class="error"><?php echo $proErr; ?></span>
         <br>
-        <input type="checkbox" id="cgu" name="cgu" value="true">
+        <input type="checkbox" id="cgu" name="cgu">
         <label for="cgu">Je reconnais avoir pris connaissance des conditions d’utilisation et y adhère totalement.</label>
+        <span class="error"><?php echo $cguErr; ?></span>
         <br>
         <button type="submit" name="submit">Submit</button>
     </form>
---
+++ sql/niveau2/Exercice 3 - Vraie page login/login.php
@@ -0,0 +1,192 @@
+<?php
+
+session_start();
+
+$emailErr = $passwordErr = "";
+
+function test_input($data)
+{
+    $data = trim($data); //Strip unnecessary characters (extra space, tab, newline)
+    $data = stripslashes($data); //Remove backslashes
+    $data = htmlspecialchars($data); //converts special characters to HTML entities to avoid malicious code injection
+    return $data;
+}
+
+function connexionsLog($email, $password, $clientIp, $succes)
+{
+    $dbUser = 'root';
+    $dbPass = '';
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=niveau2', $dbUser, $dbPass);
+        $sql = "INSERT INTO Connexions (login, password, ip, succes)
+                VALUES  ('$email', '$password', '$clientIp', '$succes')";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $dbh = null;
+    } catch (PDOException $e) {
+        echo "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+
+function emailExist($email)
+{
+    $dbUser = 'root';
+    $dbPass = '';
+
+    try {
+
+        $dbh = new PDO('mysql:host=localhost;dbname=niveau2', $dbUser, $dbPass);
+
+        $sql = "SELECT email FROM Utilisateurs WHERE email = '$email' ";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        $dbh = null;
+        return $result;
+    } catch (PDOException $e) {
+        echo "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+
+function getClientIp()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip_address;
+}
+
+function verifyIpBan()
+{
+    $dbUser = 'root';
+    $dbPass = '';
+    $clientIp = getClientIp();
+    $ban = false;
+
+    try {
+
+        $dbh = new PDO('mysql:host=localhost;dbname=niveau2', $dbUser, $dbPass);
+        $sql = "SELECT date_connexion, succes as x FROM Connexions WHERE ip = '127.0.0.1' AND TIMEDIFF(NOW(), date_connexion) < '00:30:00' ORDER BY date_connexion DESC";
+        $stmt = $dbh->query($sql);
+        $failedConn = 1;
+        foreach ($stmt as $row) {
+            if ($failedConn >= 5) {
+                $ban = true;
+                break;
+            }
+            if ($row['x'] == "1") {
+                $ban = false;
+                break;
+            }
+            $failedConn++;
+        }
+        $dbh = null;
+        return $ban;
+    } catch (PDOException $e) {
+        echo "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+
+function login($email, $password, $date)
+{
+
+    if (emailExist($email)) {
+
+        $clientIp = getClientIp();
+        $dbUser = 'root';
+        $dbPass = '';
+
+        try {
+            $dbh = new PDO('mysql:host=localhost;dbname=niveau2', $dbUser, $dbPass);
+
+            $sql = "SELECT password FROM Utilisateurs WHERE email = '$email' ";
+            $stmt = $dbh->query($sql);
+
+            foreach ($stmt as $row) {
+                $passCheck = password_verify($password, $row["password"]);
+            }
+            if ($passCheck == 1) {
+                $_SESSION['email'] = $email;
+                connexionsLog($email, $password, $clientIp, '1');
+                header('location: home.php');
+            } else {
+                connexionsLog($email, $password, $clientIp, '0');
+                echo "<script>alert(\"E-Mail, Password combination incerrect!\")</script>";
+            }
+            $dbh = null;
+        } catch (PDOException $e) {
+            echo "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    } else {
+        echo "<script>alert(\"E-Mail, Password combination incerrect!\")</script>";
+    }
+}
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = test_input($_POST["email"]);
+    $password = test_input($_POST["password"]);
+    $valid = true;
+    if (empty($email)) {
+        $emailErr = "An E-Mail is required";
+        $valid = false;
+    }
+    if (empty($password)) {
+        $passwordErr = "The password is required";
+        $valid = false;
+    }
+
+    if ($valid == true) {
+        $date = date("Y-m-d H:i:s");
+        if(!verifyIpBan()){
+            login($email, $password, $date);
+        }else{
+            echo "<script>alert(\"Too many connexions attempted. \\nFor safety you cannot login. \\nYou can try again in 30min \")</script>"; 
+        }
+        
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .error {
+            color: red;
+        }
+
+        >
+    </style>
+    <title>sql - login page</title>
+</head>
+
+<body>
+
+    <form action="login.php" method="post">
+
+        <label for="Email">E-Mail</label>
+        <input type="text" id="email" name="email" value="<?php echo isset($_POST["email"]) ? $_POST["email"] : "" ?>"><span class="error"><?php echo $emailErr; ?></span>
+        <br>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password"><span class="error"><?php echo $passwordErr; ?></span>
+        <br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+
+</body>
+
+</html>
